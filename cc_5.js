4ef468ed21c02d62cde385275efd7e6b9a2f2908
Alex White <awhite6@usf.edu>
2025-09-23 20:38:02 -0400

    First Commit (made array)

---
+++ cc_5.js
@@ -0,0 +1,17 @@
+let employees = [
+    {
+        name: "Spongebob",
+        hourlyRate: 14,
+        hoursWorked: 40
+    },
+    {
+        name: "Patrick",
+        hourlyRate: 20,
+        hoursWorked: 10
+    },
+    {
+        name: "Squidward",
+        hourlyRate: 30,
+        hoursWorked: 30
+    }
+]
