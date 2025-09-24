c3547fc32c02e0b5c89f3620d2715ffeaa4456e1
Alex White <awhite6@usf.edu>
2025-09-23 20:57:11 -0400

    Created base pay and overtime functions

--- cc_5.js
+++ cc_5.js
@@ -1,8 +1,11 @@
+// CC 5
+
+// Employees Array
 let employees = [
     {
         name: "Spongebob",
         hourlyRate: 14,
-        hoursWorked: 40
+        hoursWorked: 60
     },
     {
         name: "Patrick",
@@ -15,3 +18,20 @@ let employees = [
         hoursWorked: 30
     }
 ]
+
+// Base Pay Function
+function calculateBasePay(rate, hours) {
+    let validHours = Math.min(hours, 40)
+    return rate * validHours
+}
+
+// Overtime Pay Function
+function calculateOvertimePay(rate, hours) {
+    if (hours > 40) {
+        let overtimeHours = hours - 40;
+        return overtimeHours * (rate * 1.5);
+    }
+        else {
+        return 0;
+    }
+}
